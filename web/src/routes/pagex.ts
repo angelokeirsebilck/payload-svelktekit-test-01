69df76802135b12620048d821920576ffeafd433
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-12-26 10:21:40 +0100

    Test v

--- web/src/routes/pagex.ts
+++ web/src/routes/pagex.ts
@@ -2,5 +2,5 @@ import type { PageLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
 
 export const load = (() => {
-  throw redirect(301, "/nl");
+  // throw redirect(301, "/nl");
 }) satisfies PageLoad;
