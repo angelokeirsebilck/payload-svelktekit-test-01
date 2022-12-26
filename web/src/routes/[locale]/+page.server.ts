f30eb0292689b5388049b600d83537e4b3c87c2f
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-12-26 08:46:45 +0100

    homepage 404 test

--- web/src/routes/[locale]/+page.server.ts
+++ web/src/routes/[locale]/+page.server.ts
@@ -1,8 +1,17 @@
 import type { Home } from "$lib/types/payload-types";
 import type { PageServerLoad } from "./$types";
 import { env } from "$env/dynamic/public";
+import { error } from "@sveltejs/kit";
 
 export const load = (({ fetch, params }) => {
+  const locales = ["nl", "en"];
+
+  if (!locales.includes(params.locale)) {
+    throw error(404, {
+      message: "Not found",
+    });
+  }
+
   const getHomeData = async (): Promise<Home> => {
     const res = await fetch(
       `${env.PUBLIC_CMS_API_ENDPOINT}/globals/home?locale=${params.locale}`
