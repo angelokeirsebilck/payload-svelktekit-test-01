b8b9981fca773e205be518b08f8a4fac13c2ac80
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-12-28 10:22:00 +0100

    change email based on node_env

--- cms/src/server.ts
+++ cms/src/server.ts
@@ -4,34 +4,36 @@ import payload from "payload";
 require("dotenv").config();
 const app = express();
 
+const devEmail = {
+  fromName: "Admin",
+  fromAddress: "admin@example.com",
+  logMockCredentials: true, // Optional
+};
+const productionEmail = {
+  transportOptions: {
+    host: "smtp.postmarkapp.com",
+    auth: {
+      user: process.env.POSTMARK_TOKEN,
+      pass: process.env.POSTMARK_TOKEN,
+    },
+    port: 587,
+    secure: true, // use TLS
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
+    },
+  },
+  fromName: "Angelo",
+  fromAddress: "info@angelokeirsebilck.be",
+};
+
 // Redirect root to Admin panel
 app.get("/", (_, res) => {
   res.redirect("/admin");
 });
 // Initialize Payload
 payload.init({
-  // email: {
-  //   fromName: "Admin",
-  //   fromAddress: "admin@example.com",
-  //   logMockCredentials: true, // Optional
-  // },
-  email: {
-    transportOptions: {
-      host: "smtp.postmarkapp.com",
-      auth: {
-        user: process.env.POSTMARK_TOKEN,
-        pass: process.env.POSTMARK_TOKEN,
-      },
-      port: 587,
-      // secure: true, // use TLS
-      tls: {
-        // do not fail on invalid certs
-        rejectUnauthorized: false,
-      },
-    },
-    fromName: "Angelo",
-    fromAddress: "info@angelokeirsebilck.be",
-  },
+  email: process.env.NODE_ENV == "production" ? productionEmail : devEmail,
   secret: process.env.PAYLOAD_SECRET,
   mongoURL: process.env.MONGODB_URI,
   express: app,
