7921fe1981824270f59c504ae7bb1b85584ed151
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2023-01-25 20:21:04 +0100

    test

--- cms/src/collections/Pages.ts
+++ cms/src/collections/Pages.ts
@@ -21,13 +21,13 @@ const Pages: CollectionConfig = {
     group: "Pages",
     defaultColumns: ["pageTitle"],
     useAsTitle: "pageTitle",
-    // preview: (doc, { locale }) => {
-    //   if (doc?.uri) {
-    //     // return `https://payload-svelktekit-test-01.vercel.app/preview/pages?id=${doc.id}&locale=${locale}`;
-    //     return `${process.env.PAYLOAD_PUBLIC_WEB_URL}/preview/pages?id=${doc.id}&locale=${locale}`;
-    //   }
-    //   return null;
-    // },
+    preview: (doc, { locale }) => {
+      if (doc?.uri) {
+        // return `https://payload-svelktekit-test-01.vercel.app/preview/pages?id=${doc.id}&locale=${locale}`;
+        return `${process.env.PAYLOAD_PUBLIC_WEB_URL}/preview/pages?id=${doc.id}&locale=${locale}`;
+      }
+      return null;
+    },
   },
   versions: {
     maxPerDoc: 15,
